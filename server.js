ce301d9468588b4812c0e37c8b1c2e8af86f9d80
vinayby06-cpu <vinayby06@gmail.com>
2026-03-03 19:26:53 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -323,3 +323,11 @@ app.use((err, req, res, next) => {
 // ================== START SERVER ==================
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.use("/api", (req, res) => {
+  return res.status(404).json({
+    message: "API route not found",
+    path: req.originalUrl,
+    method: req.method,
+  });
+});
